b9f925628d13ac7e62c61a237b8018fcb4a37d50
sla <slavanovosyolov@gmail.com>
2023-01-23 23:28:33 +0300

    Add old version

---
+++ WheatherForcast/WheatherForcast/Program.cs
@@ -0,0 +1,133 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Web;
+using WheatherForcast;
+
+Dictionary<DayOfWeek, string> days = new Dictionary<DayOfWeek, string>()
+{
+    [DayOfWeek.Monday] = "Понедельник",
+    [DayOfWeek.Tuesday] = "Вторник",
+    [DayOfWeek.Wednesday] = "Среда",
+    [DayOfWeek.Thursday] = "Четверг",
+    [DayOfWeek.Friday] = "Пятница",
+    [DayOfWeek.Saturday] = "Суббота",
+    [DayOfWeek.Sunday] = "Воскресенье"
+};
+
+//-------------------------------------------------------------------------------------
+Console.OutputEncoding = Encoding.UTF8;
+var apiKey = "c87d180368ccb05488b4db3805db2762";
+var client = new HttpClient();
+
+while (true)
+{
+    Console.Write("Прогноз погоды! Введите город: ");
+    var city = Console.ReadLine();
+    Console.WriteLine("Выясняем, подождите пожалуйста");
+    var response = await client.GetAsync(@$"https://api.openweathermap.org/data/2.5/forecast?q={HttpUtility.UrlEncode(city)}&appid={apiKey}&units=metric&lang=ru");
+
+    if (response.IsSuccessStatusCode)
+    {
+        var result = await response.Content.ReadAsStringAsync();
+        var info = JsonConvert.DeserializeObject<WheatherInfo>(result);
+        Console.Clear();
+        Console.WriteLine(
+            $"Погода в городе {info.City.Name}, {info.City.Country} на {DateTime.Now} - {info.List[0].Weather[0].Description}\n" +
+            $"Температура воздуха - {Math.Round(info.List[0].Main.Temp, 1)}°С\n" +
+            $"А по ощущениям - {Math.Round(info.List[0].Main.Feels_like, 1)}°С\n" +
+            $"Ветер - {info.List[0].Wind.Speed}м/с, {GetWind(info.List[0].Wind.Deg)}\n" +
+            $"Влажность - {info.List[0].Main.Humidity}%\n" +
+            $"Давление - {Math.Round(info.List[0].Main.Grnd_level / 1.33322, 2)} мм рт. ст. (нормальное давление - 760 мм рт. ст.)\n\n" +
+            $"Прогноз погоды на 4 дня:\n");
+        int nextDayIter = 8;
+        DateTime iterDate;
+        for (int i = 0; i < 4; i++)
+        {
+            iterDate = DateTime.Parse(info.List[nextDayIter].Dt_txt);
+            Console.Write($"{iterDate.ToShortDateString(), 25} | ");
+            nextDayIter += 8;
+        }
+        Console.WriteLine();
+        nextDayIter = 8;
+        for (int i = 0; i < 4; i++)
+        {
+            iterDate = DateTime.Parse(info.List[nextDayIter].Dt_txt);
+            Console.Write($"{days[iterDate.DayOfWeek], 25} | ");
+            nextDayIter += 8;
+        }
+        Console.WriteLine();
+        nextDayIter = 8;
+        for (int i = 0; i < 4; i++)
+        {
+            //Temp_min == Temp_max ?
+            string temp = $"{Math.Round(info.List[nextDayIter].Main.Temp_min, 2)}...{Math.Round(info.List[nextDayIter].Main.Temp_max, 2)}";
+            Console.Write($"{temp, 25} | ");
+            nextDayIter += 8;
+        }
+        Console.WriteLine();
+        nextDayIter = 8;
+        for (int i = 0; i < 4; i++)
+        {
+            string wheatherDesc = info.List[nextDayIter].Weather[0].Description
+                .Substring(0, 1)
+                .ToUpper()
+                + info.List[nextDayIter].Weather[0].Description
+                .Substring(1, info.List[nextDayIter].Weather[0].Description.Length - 1);
+            Console.Write($"{wheatherDesc, 25} | ");
+            nextDayIter += 8;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Не верно указан город!");
+    }
+
+    Console.WriteLine("\n\n\nНажмите ENTER для повторного запроса прогноза погоды");
+    Console.ReadLine();
+    Console.Clear();
+}
+
+
+
+//----------------------------------------------------------------------------
+
+
+string GetWind(int deg)
+{
+    if (deg > 345 && deg <= 360 || deg >= 0 && deg < 15)
+    {
+        return "северный";
+    }
+    else if (deg >= 15 && deg <= 75)
+    {
+        return "северо-восточный";
+    }
+    else if (deg > 75 && deg < 105)
+    {
+        return "восточный";
+    }
+    else if (deg >= 105 && deg <= 165)
+    {
+        return "юго-восточный";
+    }
+    else if (deg > 165 && deg < 195)
+    {
+        return "южный";
+    }
+    else if (deg >= 195 && deg <= 255)
+    {
+        return "юго-западный";
+    }
+    else if (deg > 255 && deg < 285)
+    {
+        return "западный";
+    }
+    else if (deg >= 285 && deg <= 345)
+    {
+        return "северо-западный";
+    }
+    else
+    {
+        return "???";
+    }
+}
---
+++ WheatherForcast/WheatherForcast/WheatherInfo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheatherForcast
+{
+    public class WheatherInfo
+    {
+        public string Cod { get; set; }
+        public int Message { get; set; }
+        public int Cnt { get; set; }
+        public List[] List { get; set; }
+        public City City { get; set; }
+    }
+
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Coord Coord { get; set; }
+        public string Country { get; set; }
+        public int Population { get; set; }
+        public int Timezone { get; set; }
+        public int Sunrise { get; set; }
+        public int Sunset { get; set; }
+    }
+
+    public class Coord
+    {
+        public float Lat { get; set; }
+        public float Lon { get; set; }
+    }
+
+    public class List
+    {
+        public int Dt { get; set; }
+        public Main Main { get; set; }
+        public Weather[] Weather { get; set; }
+        public Clouds Clouds { get; set; }
+        public Wind Wind { get; set; }
+        public int Visibility { get; set; }
+        public double Pop { get; set; }
+        public Sys Sys { get; set; }
+        public string Dt_txt { get; set; }
+    }
+
+    public class Main
+    {
+        public float Temp { get; set; }
+        public float Feels_like { get; set; }
+        public float Temp_min { get; set; }
+        public float Temp_max { get; set; }
+        public int Pressure { get; set; }
+        public int Sea_level { get; set; }
+        public int Grnd_level { get; set; }
+        public int Humidity { get; set; }
+        public float Temp_kf { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int All { get; set; }
+    }
+
+    public class Wind
+    {
+        public float Speed { get; set; }
+        public int Deg { get; set; }
+        public float Gust { get; set; }
+    }
+
+    public class Sys
+    {
+        public string Pod { get; set; }
+    }
+
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
