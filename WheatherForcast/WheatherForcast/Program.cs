94cd33aadb48b671b1dfe97a7fdb247fb18ef967
sla <slavanovosyolov@gmail.com>
2023-01-24 14:44:57 +0300

    Code Refactoring
    Переработка вывода прогноза на 4 дня, перевода дней недели и направления ветра

--- WheatherForcast/WheatherForcast/Program.cs
+++ WheatherForcast/WheatherForcast/Program.cs
@@ -3,18 +3,8 @@ using System.Text;
 using System.Web;
 using WheatherForcast;
 
-Dictionary<DayOfWeek, string> days = new Dictionary<DayOfWeek, string>()
-{
-    [DayOfWeek.Monday] = "Понедельник",
-    [DayOfWeek.Tuesday] = "Вторник",
-    [DayOfWeek.Wednesday] = "Среда",
-    [DayOfWeek.Thursday] = "Четверг",
-    [DayOfWeek.Friday] = "Пятница",
-    [DayOfWeek.Saturday] = "Суббота",
-    [DayOfWeek.Sunday] = "Воскресенье"
-};
-
 //-------------------------------------------------------------------------------------
+
 Console.OutputEncoding = Encoding.UTF8;
 var apiKey = "c87d180368ccb05488b4db3805db2762";
 var client = new HttpClient();
@@ -25,7 +15,6 @@ while (true)
     var city = Console.ReadLine();
     Console.WriteLine("Выясняем, подождите пожалуйста");
     var response = await client.GetAsync(@$"https://api.openweathermap.org/data/2.5/forecast?q={HttpUtility.UrlEncode(city)}&appid={apiKey}&units=metric&lang=ru");
-
     if (response.IsSuccessStatusCode)
     {
         var result = await response.Content.ReadAsStringAsync();
@@ -39,95 +28,72 @@ while (true)
             $"Влажность - {info.List[0].Main.Humidity}%\n" +
             $"Давление - {Math.Round(info.List[0].Main.Grnd_level / 1.33322, 2)} мм рт. ст. (нормальное давление - 760 мм рт. ст.)\n\n" +
             $"Прогноз погоды на 4 дня:\n");
+        int cursorX = Console.GetCursorPosition().Left;
+        int cursorY = Console.GetCursorPosition().Top;
         int nextDayIter = 8;
+        string infoString;
         DateTime iterDate;
+        List currentDateWeather;
         for (int i = 0; i < 4; i++)
         {
-            iterDate = DateTime.Parse(info.List[nextDayIter].Dt_txt);
-            Console.Write($"{iterDate.ToShortDateString(), 25} | ");
-            nextDayIter += 8;
-        }
-        Console.WriteLine();
-        nextDayIter = 8;
-        for (int i = 0; i < 4; i++)
-        {
-            iterDate = DateTime.Parse(info.List[nextDayIter].Dt_txt);
-            Console.Write($"{days[iterDate.DayOfWeek], 25} | ");
-            nextDayIter += 8;
-        }
-        Console.WriteLine();
-        nextDayIter = 8;
-        for (int i = 0; i < 4; i++)
-        {
+            currentDateWeather = info.List[nextDayIter];
+            // Дата
+            iterDate = DateTime.Parse(currentDateWeather.Dt_txt);
+            Console.Write($"{iterDate.ToShortDateString(),25} | ");
+            Console.SetCursorPosition(cursorX, cursorY + 1);
+            // День недели
+            infoString = iterDate.ToString("dddd");
+            infoString = infoString[0].ToString().ToUpper() + infoString.Substring(1);
+            Console.Write($"{infoString,25} | ");
+            Console.SetCursorPosition(cursorX, cursorY + 2);
+            // Температура
             //Temp_min == Temp_max ?
-            string temp = $"{Math.Round(info.List[nextDayIter].Main.Temp_min, 2)}...{Math.Round(info.List[nextDayIter].Main.Temp_max, 2)}";
-            Console.Write($"{temp, 25} | ");
+            infoString = $"{Math.Round(currentDateWeather.Main.Temp_min, 2)}...{Math.Round(currentDateWeather.Main.Temp_max, 2)}";
+            Console.Write($"{infoString,25} | ");
+            Console.SetCursorPosition(cursorX, cursorY + 3);
+            // Описание погоды
+            infoString = currentDateWeather.Weather[0].Description;
+            infoString = infoString[0].ToString().ToUpper() + infoString.Substring(1);
+            Console.Write($"{infoString,25} | ");
+            cursorX = Console.GetCursorPosition().Left;
+            Console.SetCursorPosition(cursorX, cursorY);
+
             nextDayIter += 8;
         }
-        Console.WriteLine();
-        nextDayIter = 8;
-        for (int i = 0; i < 4; i++)
-        {
-            string wheatherDesc = info.List[nextDayIter].Weather[0].Description
-                .Substring(0, 1)
-                .ToUpper()
-                + info.List[nextDayIter].Weather[0].Description
-                .Substring(1, info.List[nextDayIter].Weather[0].Description.Length - 1);
-            Console.Write($"{wheatherDesc, 25} | ");
-            nextDayIter += 8;
-        }
+        Console.SetCursorPosition(0, cursorY + 6);
     }
     else
     {
         Console.WriteLine("Не верно указан город!");
     }
-
-    Console.WriteLine("\n\n\nНажмите ENTER для повторного запроса прогноза погоды");
+    Console.WriteLine("Нажмите ENTER для повторного запроса прогноза погоды");
     Console.ReadLine();
     Console.Clear();
 }
 
-
-
 //----------------------------------------------------------------------------
-
 
 string GetWind(int deg)
 {
-    if (deg > 345 && deg <= 360 || deg >= 0 && deg < 15)
+    switch (deg)
     {
-        return "северный";
-    }
-    else if (deg >= 15 && deg <= 75)
-    {
-        return "северо-восточный";
-    }
-    else if (deg > 75 && deg < 105)
-    {
-        return "восточный";
-    }
-    else if (deg >= 105 && deg <= 165)
-    {
-        return "юго-восточный";
-    }
-    else if (deg > 165 && deg < 195)
-    {
-        return "южный";
-    }
-    else if (deg >= 195 && deg <= 255)
-    {
-        return "юго-западный";
-    }
-    else if (deg > 255 && deg < 285)
-    {
-        return "западный";
-    }
-    else if (deg >= 285 && deg <= 345)
-    {
-        return "северо-западный";
-    }
-    else
-    {
-        return "???";
+        case int when deg > 345 && deg <= 360 || deg >= 0 && deg < 15:
+            return "северный";
+        case int when deg >= 15 && deg <= 75:
+            return "северо-восточный";
+        case int when deg > 75 && deg < 105:
+            return "восточный";
+        case int when deg >= 105 && deg <= 165:
+            return "юго-восточный";
+        case int when deg > 165 && deg < 195:
+            return "южный";
+        case int when deg >= 195 && deg <= 255:
+            return "юго-западный";
+        case int when deg > 255 && deg < 285:
+            return "западный";
+        case int when deg >= 285 && deg <= 345:
+            return "северо-западный";
+        default:
+            return "???";
     }
 }
